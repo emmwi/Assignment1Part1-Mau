9e14ae2be4bc17e3457783647f6ecaafe7944b96
Emma <emma.m.with@gmail.com>
2025-09-10 11:19:53 +0200

    adding files

---
+++ Assignment1Part1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment1Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
